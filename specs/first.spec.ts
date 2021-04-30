52eef2c1736a1a2f56b469b2e21c1547a31e13d8
Philip Rozycki-Burn <pr@neoftl.com>
2021-04-30 19:31:40 +0100

    Removed unnecessary spec changes

--- specs/first.spec.ts
+++ specs/first.spec.ts
@@ -1,25 +1,14 @@
 import * as chai from 'chai';
 const expect = chai.expect;
 
-const sleep = m => new Promise(r => setTimeout(r, m));
-
 context('Initial suite', function () {
 
 	it('A first basic test', async function () {
 
 		this.timeout(10000);
 
-		const waitTime: Number = 5000;
-
-		console.log(new Date().toDateString());
-		await delay(waitTime);
-
-		expect(1).to.be.equal(1);
+		expect(1).to.equal(1);
 
 	});
 
 });
-
-async function delay(time) {
-	await sleep(time);
-}
