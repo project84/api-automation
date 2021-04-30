0193f79ac7ee9696944f46d45df7ca7767cf90e9
Philip Rozycki-Burn <pr@neoftl.com>
2021-04-30 07:20:38 +0100

    Added basic spec file to confirm setup

---
+++ specs/first.spec.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+context('Initial suite', function () {
+
+	it('A first basic test', function () {
+
+		expect(1).to.equal(1);
+
+	});
+
+});
