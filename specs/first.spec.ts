4358738ca51eb5d3f7222159cb37c265d455d3e0
Philip Rozycki-Burn <pr@neoftl.com>
2021-04-30 19:45:43 +0100

    Removed whitespace and timeout

--- specs/first.spec.ts
+++ specs/first.spec.ts
@@ -1,13 +1,9 @@
 import * as chai from 'chai';
 const expect = chai.expect;
 
-
-
 context('Initial suite', function () {
 
 	it('A first basic test', async function () {
-
-		this.timeout(10000);
 
 		expect(1).to.equal(1);
 
