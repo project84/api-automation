94a094af153021ad057923896e8d6f5dff6b4293
Philip Rozycki-Burn <pr@neoftl.com>
2021-05-09 19:09:01 +0100

    Chai plugin for schema validation

--- support/custom-assertions/index.d.ts
+++ support/custom-assertions/index.d.ts
@@ -1,9 +1,16 @@
+import { Context } from 'mocha';
+
 /* eslint-disable no-unused-vars */
 declare global {
 	namespace Chai {
 		interface Assertion {
-			status(expStatus: number): Assertion;
-			header(expHeader: string): Assertion;
+			// API methods
+			status(expectedStatus: number): Assertion;
+			header(expectedHeader: string): Assertion;
+
+			// Schema validation
+			conform: Assertion;
+			schema(mocha: Context, schemaPath: string, isArray?: boolean): Assertion;
 		}
 	}
 }
---
+++ support/custom-assertions/schema-validation.ts
@@ -0,0 +1,26 @@
+import { validate } from '@support/validators/schema';
+import { Context } from 'mocha';
+
+module.exports = function (chai) {
+
+	let Assertion = chai.Assertion;
+
+	Assertion.addProperty('conform');
+
+	/**
+	 * Validate assertion target against provided schema
+	 */
+	Assertion.addMethod('schema', function(mocha: Context, schemaPath: string, isArray?: boolean) {
+
+		const result = validate(mocha, this._obj, schemaPath, isArray);
+		const error = result.errors[0];
+
+		this.assert(
+			result.isValid,
+			`Expected object to conform to schema: ${error.instancePath} ${error.message} (actual: ${error.data})`,
+			null
+		);
+
+	});
+
+};
