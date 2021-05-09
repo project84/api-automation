fc822552792ff036466f64cddc82fe54575a149e
Philip Rozycki-Burn <pr@neoftl.com>
2021-05-09 19:15:00 +0100

    Added schema validation to spec

--- specs/pizza/orders.spec.ts
+++ specs/pizza/orders.spec.ts
@@ -1,7 +1,6 @@
 import { expect } from 'chai';
 
 import { createOrder, deleteOrder, getOrders } from '@modules/pizza/orders';
-import { FetchResponse } from '@models/support/fetch';
 import { Pizza } from '@models/pizza/pizza';
 
 context('Orders', function () {
@@ -9,8 +8,11 @@ context('Orders', function () {
 	it('A user must be able to retrieve all pizza orders', async function () {
 
 		// Retrieve pizza order list and verify response status
-		const res: FetchResponse = await getOrders(this);
+		const res = await getOrders(this);
 		expect(res).to.have.status(200);
+
+		// Validate response body against schema
+		expect(res.body).to.conform.to.schema(this, 'pizza/order', true);
 
 	});
 
@@ -26,8 +28,11 @@ context('Orders', function () {
 		};
 
 		// Create new pizza order and verify response status
-		const res: FetchResponse = await createOrder(this, pizza);
+		const res = await createOrder(this, pizza);
 		expect(res).to.have.status(200);
+
+		// Validate response body against schema
+		expect(res.body).to.conform.to.schema(this, 'pizza/order');
 
 	});
 
@@ -35,8 +40,11 @@ context('Orders', function () {
 	it.skip('A user must be able to delete a pizza order', async function () {
 
 		// Delete pizza order and verify response status
-		const res: FetchResponse = await deleteOrder(this, 1);
+		const res = await deleteOrder(this, 1);
 		expect(res).to.have.status(200);
+
+		// Validate response body against schema
+		expect(res.body).to.conform.to.schema(this, 'pizza/order-deletion');
 
 		// Verify that the order was deleted successfully
 		expect(res.body?.message).to.be.equal('Order deleted');
